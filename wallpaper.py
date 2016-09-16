acf44f40d831519eb580d0ab9160291e1b67da62
Eugenio <emej92@gmail.com>
2016-09-16 06:27:16 -0400

    Added command line options. Now only opens the number of images required by the layout. Now generates a more accurate text file.

--- wallpaper.py
+++ wallpaper.py
@@ -169,7 +169,7 @@ def determine_size(code, nxsize, nysize):
 		print('Error: Image has no size associated.')
 	return size;
 
-def create_size_matrix(images, layout):
+def create_size_matrix(layout):
 	rows = len(layout)
 	cols = len(layout[0])
 	size_matrix = []
@@ -214,6 +214,7 @@ def get_images(folder_path, num):
 		# print(img.verify())
 		# images.append(load_image(path.join(folder_path, file_names[r])))
 		images.append(Image.open(path.join(folder_path, file_names[r])))
+		file.write("<" + "{:>02}".format(str(i+1)) + "> ")
 		file.write(file_names[r]+'\n')
 		if len(file_names) > 1:
 			file_names.pop(r)
@@ -250,13 +251,15 @@ def threaded_images(images, size_matrix, ratio):
 
 	error_cnt = 0
 
+	img_cnt = 0
+
 	for y in range(0, int(nysize*ydiv/ratio), nysize):
 		for x in range(0, nxsize*xdiv, nxsize):
 			if size_matrix[cnt] != (0, 0):
 				success = False
 				while not success:
 					try:
-						img = ImageOps.fit(images[cnt], size_matrix[cnt], Image.ANTIALIAS, bleed = border, centering = (0.5, 0.5))
+						img = ImageOps.fit(images[img_cnt], size_matrix[cnt], Image.ANTIALIAS, bleed = border, centering = (0.5, 0.5))
 						#print(size_matrix[cnt])
 
 
@@ -289,6 +292,7 @@ def threaded_images(images, size_matrix, ratio):
 
 
 						success = True
+						img_cnt = img_cnt + 1
 						error_cnt = 0
 					except:
 						error_cnt = error_cnt + 1
@@ -353,63 +357,65 @@ def batch(path):
 	bg2 = Image.new('RGBA', screen_size, (255,255,255,0))
 	bg3 = Image.new('RGBA', screen_size, (255,255,255,0))
 
-	# Get files
-
-	# file_names = get_files(test_path)
-	# print(file_names)
-
-	images = get_images(folder_path, 50);
+	
 	lay = love();
-	# print(len(lay.l))
 
-	#size_matrix = create_size_matrix(images, layout3)
-	size_matrix = create_size_matrix(images, lay.l)
+	size_matrix = create_size_matrix(lay.l)
+	# images = get_images(folder_path, sum(1 for s in size_matrix if s != (0, 0)));
 
-	# print(len(size_matrix))
 
+
+	# Find how many sizes each thread will take care of
 	s1 = int(len(size_matrix)/3)
-	s2 = s1+1
-	s3 = s1*2
 
-	m1 = size_matrix[:s3]
+	# Split the matrix into equal sizes
+	m1 = size_matrix[:s1]
+	m2 = size_matrix[s1:s1*2]
+	m3 = size_matrix[s1*2:]
 
-	m2 = size_matrix[s1:]
-	m1 = size_matrix[:s2]
-	m3 = size_matrix[s3:]
+	# Find the non-zero size images for each section
+	nz1 = sum(1 for s in m1 if s != (0, 0))
+	nz2 = sum(1 for s in m2 if s != (0, 0))
+	nz3 = sum(1 for s in m3 if s != (0, 0))
 
-	thread1 = wp_thread(1, 'Thread-1', 1, images[s1:], m1, 3)
-	thread2 = wp_thread(2, 'Thread-2', 2, images[:s2], m2, 3)
-	thread3 = wp_thread(3, 'Thread-3', 3, images[s3:], m3, 3)
+	i1 = get_images(folder_path, nz1);
+	file.write(str(m1) + '\n')
+	i2 = get_images(folder_path, nz2);
+	file.write(str(m2) + '\n')
+	i3 = get_images(folder_path, nz3);
+	file.write(str(m3) + '\n')
 
-	file.write(str(size_matrix))
-	file.close()
+	# Prepare threads
+	# thread1 = wp_thread(1, 'Thread-1', 1, images[:nz1], m1, 3)
+	# thread2 = wp_thread(2, 'Thread-2', 2, images[nz1:nz1+nz2], m2, 3)
+	# thread3 = wp_thread(3, 'Thread-3', 3, images[nz1+nz2:], m3, 3)
 
-	# m1 = size_matrix[:32]
+	thread1 = wp_thread(1, 'Thread-1', 1, i1, m1, 3)
+	thread2 = wp_thread(2, 'Thread-2', 2, i2, m2, 3)
+	thread3 = wp_thread(3, 'Thread-3', 3, i3, m3, 3)
 
-	# m2 = size_matrix[16:]
-	# m1 = size_matrix[:17]
-	# m3 = size_matrix[32:]
+	# Append size matrix to the text file
+	# file.write(str(size_matrix))
+	# file.close()
 
-	# thread1 = wp_thread(1, 'Thread-1', 1, images[16:], m1, 3)
-	# thread2 = wp_thread(2, 'Thread-2', 2, images[:17], m2, 3)
-	# thread3 = wp_thread(3, 'Thread-3', 3, images[32:], m3, 3)
-
+	# Start threads
 	try:
 		thread3.start()
 		thread2.start()
 		thread1.start()
 	except:
-		print("My Error.")
+		print("Error: Threads won't start.")
 
 	t3 = thread3.join()
 	t2 = thread2.join()
 	t1 = thread1.join()
 
+	# Paste the image contributions from each thread
 	bg1.paste(t1, (0,0))
 	bg2.paste(t2, (0,int(nysize*2)))
 	bg3.paste(t3, (0,int(nysize*4)))
 
-
+	# Dont remember... X_x
 	bg2 = ImageChops.multiply(bg3, bg2)
 	bg1 = ImageChops.multiply(bg1, bg2)
 
@@ -417,10 +423,29 @@ def batch(path):
 	# bg1.save(path, 'JPEG', quality=100)
 	bg1.convert('RGB').save(path, 'PNG')
 
-	# system('feh --bg-fill /home/meiji/Clouds/Dropbox/Programming/Python/saved_image.png')
-system('feh --bg-fill ' + path.expanduser(save_path+ "saved_image" + ".png"))
-print('Working...')
-for n in range(0, 1):
-	batch(path.expanduser(save_path+ "saved_image" + ".png"))
-print('Done!')
-# batch(save_path)
+def main():
+	global folder_path
+	wait = 1;
+	if len(sys.argv) < 2:
+		folder_path = '/home/meiji/Pictures/wally/general/'
+	elif len(sys.argv) < 3:
+		folder_path = sys.argv[1]
+	else:
+		folder_path = sys.argv[1]
+		wait = int(sys.argv[2])
+	
+	if not wait:
+		system('feh --bg-fill ' + path.expanduser(save_path+ "saved_image" + ".png"))
+
+	print('Working...')
+	for n in range(0, 1):
+		batch(path.expanduser(save_path+ "saved_image" + ".png"))
+	print('Done!')
+
+	if wait:
+		system('feh --bg-fill ' + path.expanduser(save_path+ "saved_image" + ".png"))
+	# batch(save_path)
+
+
+if __name__ == '__main__':
+	main()
